589ff22be3c80cb253c560fa75660973321ce3b3
dmisav <dmitry.savchenko@miratechgroup.com>
2014-10-17 14:58:28 +0300

    Added project

---
+++ FrontPagePackages/FrontPagePackages/Entities/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace FrontPagePackages.Entities
+{
+    public class Item : IDisplayableName
+    {
+        public Item(string displayName, string toolTieText)
+        {
+            this.DisplayName = displayName;
+            this.ToolTieText = toolTieText;
+        }
+
+        public string DisplayName { get; private set; }
+
+        public string ToolTieText { get; private set; }
+    }
+}
---
+++ FrontPagePackages/FrontPagePackages/Entities/Package.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace FrontPagePackages.Entities
+{
+    public class Package : IDisplayableName
+    {
+        public Package(string displayName)
+        {
+            this.DisplayName = displayName;
+        }
+
+        public string DisplayName { get; private set; }
+
+        public List<Item> items { get; set; }
+    }
+}
